56deb1931a324eac988350ceeefa046c0059834f
markgerrard <mark@wiremock.io>
2024-07-08 11:56:56 +0100

    button style tweaks

--- src/components/forms/button.tsx
+++ src/components/forms/button.tsx
@@ -8,7 +8,7 @@ const Button: ParentComponent<{
   return (
     <button
       type="button"
-      class="flex gap-x-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+      class="flex gap-x-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       classList={{ 'bg-blue-300': props.disabled }}
       {...props}
     >
