889c51665d65d7122b2031675f44fd4140d3bb1f
markgerrard <mark@wiremock.io>
2024-07-08 17:54:15 +0100

    tweak footer and styles

--- src/App.tsx
+++ src/App.tsx
@@ -26,10 +26,10 @@ const App: ParentComponent = (props) => {
         {props.children}
       </main>
 
-      <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+      <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800 max-w-screen-xl mx-auto">
         <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2024
+            © 2024{' '}
             <a href="https://wiremock.io/" class="hover:underline">
               Developed and powered by WireMock
             </a>
